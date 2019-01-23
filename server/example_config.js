476ba95682bf6289b5c5b67efd937011a8c47645
Damian <dugandamian@gmail.com>
2019-01-23 16:17:24 +0100

    test

--- server/example_config.js
+++ server/example_config.js
@@ -1,7 +1,7 @@
 const server = "ds163694.mlab.com:63694";
 const database = "instapic";
-const user = "root";
-const password = "rootroot1";
+const user = "";
+const password = "";
 
 const db = `mongodb://${user}:${password}@${server}/${database}`;
 
