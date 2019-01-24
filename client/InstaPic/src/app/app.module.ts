f6b5b8678685f71894bb26e137bf4d65e89f38c1
jmevel <jmevel.mevel@coding-academy.fr>
2019-01-24 19:13:21 +0100

    PictureComponent Angular

--- client/InstaPic/src/app/app.module.ts
+++ client/InstaPic/src/app/app.module.ts
@@ -1,16 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { PictureCreateComponent } from './picture-components/picture-create/picture-create.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, PictureCreateComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
