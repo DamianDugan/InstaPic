53b8f54afcbae0b24e4dcabe64ee609f65e7f5ac
jmevel <jmevel.mevel@coding-academy.fr>
2019-01-25 11:52:32 +0100

    Form picture post

--- client/InstaPic/src/app/app.module.ts
+++ client/InstaPic/src/app/app.module.ts
@@ -1,37 +1,30 @@
-<<<<<<< HEAD
+// Built-in imports
+import { AppComponent } from './app.component';
+import { PictureCreateComponent } from './picture-components/picture-create/picture-create.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
-import { AppComponent } from './app.component';
-import { PictureCreateComponent } from './picture-components/picture-create/picture-create.component';
-
-@NgModule({
-  declarations: [AppComponent, PictureCreateComponent],
-  imports: [BrowserModule, FormsModule],
-=======
-//Built-in imports
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { FormsModule } from "@angular/forms";
-import { RouterModule } from "@angular/router";
-import { HttpClientModule } from "@angular/common/http";
-//Components imports
-import { AppComponent } from "./app.component";
-import { UserComponent } from "./user/user.component";
-import { SignUpComponent } from "./user/sign-up/sign-up.component";
-//Routes
-import { appRoutes } from "./routes";
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+// Components imports
+import { UserComponent } from './user/user.component';
+import { SignUpComponent } from './user/sign-up/sign-up.component';
+// Routes
+import { appRoutes } from './routes';
 
 @NgModule({
-  declarations: [AppComponent, UserComponent, SignUpComponent],
+  declarations: [
+    AppComponent,
+    UserComponent,
+    SignUpComponent,
+    PictureCreateComponent
+  ],
   imports: [
     BrowserModule,
     FormsModule,
     RouterModule.forRoot(appRoutes),
     HttpClientModule
   ],
->>>>>>> 0d4a9f6480194148adb36c40b03c90c7fb08c73d
   providers: [],
   bootstrap: [AppComponent]
 })
