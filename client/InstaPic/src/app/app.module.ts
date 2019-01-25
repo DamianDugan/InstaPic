f01f466b319e3f08681811552d52946f17a76db9
jmevel <jmevel.mevel@coding-academy.fr>
2019-01-25 14:44:21 +0100

    Modif nav bar et render form postpicture

--- client/InstaPic/src/app/app.module.ts
+++ client/InstaPic/src/app/app.module.ts
@@ -2,7 +2,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 // Components imports
 import { PictureCreateComponent } from './picture-components/picture-create/picture-create.component';
@@ -10,7 +9,7 @@ import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
 import { SignUpComponent } from './user/sign-up/sign-up.component';
 // Routes
-import { appRoutes } from './routes';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -19,12 +18,7 @@ import { appRoutes } from './routes';
     SignUpComponent,
     PictureCreateComponent
   ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    RouterModule.forRoot(appRoutes),
-    HttpClientModule
-  ],
+  imports: [BrowserModule, FormsModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
