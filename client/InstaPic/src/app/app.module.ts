bb77e487622960b119f6b4eb0753e00b32f39177
Damian <dugandamian@gmail.com>
2019-01-25 11:58:22 +0100

    Merge conflcit de app.module.ts

--- client/InstaPic/src/app/app.module.ts
+++ client/InstaPic/src/app/app.module.ts
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-
-import { AppComponent } from './app.component';
-import { PictureCreateComponent } from './picture-components/picture-create/picture-create.component';
-
-@NgModule({
-  declarations: [AppComponent, PictureCreateComponent],
-  imports: [BrowserModule, FormsModule],
-=======
 //Built-in imports
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
@@ -17,6 +5,7 @@ import { FormsModule } from "@angular/forms";
 import { RouterModule } from "@angular/router";
 import { HttpClientModule } from "@angular/common/http";
 //Components imports
+import { PictureCreateComponent } from "./picture-components/picture-create/picture-create.component";
 import { AppComponent } from "./app.component";
 import { UserComponent } from "./user/user.component";
 import { SignUpComponent } from "./user/sign-up/sign-up.component";
@@ -24,14 +13,18 @@ import { SignUpComponent } from "./user/sign-up/sign-up.component";
 import { appRoutes } from "./routes";
 
 @NgModule({
-  declarations: [AppComponent, UserComponent, SignUpComponent],
+  declarations: [
+    AppComponent,
+    UserComponent,
+    SignUpComponent,
+    PictureCreateComponent
+  ],
   imports: [
     BrowserModule,
     FormsModule,
     RouterModule.forRoot(appRoutes),
     HttpClientModule
   ],
->>>>>>> 0d4a9f6480194148adb36c40b03c90c7fb08c73d
   providers: [],
   bootstrap: [AppComponent]
 })
