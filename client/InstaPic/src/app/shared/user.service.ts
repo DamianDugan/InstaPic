b22c7d0bb62af01eebe7cda99ff2b42c4427fbc4
jmevel <jmevel.mevel@coding-academy.fr>
2019-01-28 12:27:15 +0100

    Fix profile form

--- client/InstaPic/src/app/shared/user.service.ts
+++ client/InstaPic/src/app/shared/user.service.ts
@@ -1,22 +1,22 @@
-import { Injectable } from "@angular/core";
-import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
-import { environment } from "../../environments/environment";
-import { User } from "./user.model";
+import { environment } from '../../environments/environment';
+import { User } from './user.model';
 
 @Injectable({
-  providedIn: "root"
+  providedIn: 'root'
 })
 export class UserService {
   selectedUser: User = {
-    username: "",
-    email: "",
-    password: "",
-    confirm_password: ""
+    username: '',
+    email: '',
+    password: '',
+    confirm_password: ''
   };
   constructor(private http: HttpClient) {}
 
   postUser(user: User) {
-    return this.http.post(environment.apiBaseUrl + "/user/register", user);
+    return this.http.post(environment.apiBaseUrl + '/user/register', user);
   }
 }
