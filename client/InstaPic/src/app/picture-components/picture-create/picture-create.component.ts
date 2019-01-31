b04094bba32f4d1a9d2a557ebd714522d4c16f16
Damian <dugandamian@gmail.com>
2019-01-31 13:25:31 +0100

    merge

--- client/InstaPic/src/app/picture-components/picture-create/picture-create.component.ts
+++ client/InstaPic/src/app/picture-components/picture-create/picture-create.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { NgForm } from '@angular/forms';
-import { PictureService } from './picture.service';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { NgForm } from "@angular/forms";
+import { PictureService } from "./picture.service";
+let EXIF = require("exif-js");
 
 @Component({
-  selector: 'app-picture-create',
-  templateUrl: './picture-create.component.html',
-  styleUrls: ['./picture-create.component.css'],
+  selector: "app-picture-create",
+  templateUrl: "./picture-create.component.html",
+  styleUrls: ["./picture-create.component.css"],
   providers: [PictureService]
 })
 export class PictureCreateComponent implements OnInit {
@@ -16,21 +17,34 @@ export class PictureCreateComponent implements OnInit {
 
   ngOnInit() {}
 
+  getExif(image) {
+    EXIF.getData(image, function() {
+      let make = EXIF.getTag(this, "Make");
+      let model = EXIF.getTag(this, "Model");
+      console.log(make, model);
+    });
+
+    EXIF.getData(image, function() {
+      let allMetaData = EXIF.getAllTags(this);
+      console.log(allMetaData);
+    });
+  }
+
   onSubmit(form: NgForm) {
     this.pictureService.postPicture(form.value).subscribe(
       res => {
-        // console.log(form.value);
+        console.log("form value: " + form.value.image);
         this.showSuccessMessage = true;
       },
       err => {
         if (err) {
-          this.serverErrorMessages = err + '<br/>';
+          this.serverErrorMessages = err + "<br/>";
         } else {
           this.serverErrorMessages =
-            'Something went wrong. Please contact admin';
+            "Something went wrong. Please contact admin";
         }
       }
     );
-    this.router.navigate(['/home']);
+    this.router.navigate(["/home"]);
   }
 }
