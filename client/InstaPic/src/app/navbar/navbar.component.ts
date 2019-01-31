d009dbd5a5984955b3b1b0bca8dac043851907ce
BeverleyB <beverley.beranger@gmail.com>
2019-01-31 17:37:28 +0100

    Add feature for view the user profile and implement feature for navbar like isAdmin & isLoggued

--- client/InstaPic/src/app/navbar/navbar.component.ts
+++ client/InstaPic/src/app/navbar/navbar.component.ts
@@ -1,12 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { UserService } from "../shared/user.service";
+import { JwtHelperService } from "@auth0/angular-jwt";
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  selector: "app-navbar",
+  templateUrl: "./navbar.component.html",
+  styleUrls: ["./navbar.component.css"]
 })
 export class NavbarComponent implements OnInit {
-  constructor() {}
+  helper = new JwtHelperService();
+  constructor(private userService: UserService) {}
 
   ngOnInit() {}
+
+  loggued() {
+    var token = this.userService.getToken();
+
+    if (token) return true;
+    else return false;
+  }
+
+  isAdmin() {
+    var token = this.userService.getToken();
+    if (token) {
+      var decodeToken = this.helper.decodeToken(token);
+      var adminDecode = decodeToken.isAdmin;
+      if (adminDecode == true) return true;
+      else return false;
+    } else return false;
+  }
 }
