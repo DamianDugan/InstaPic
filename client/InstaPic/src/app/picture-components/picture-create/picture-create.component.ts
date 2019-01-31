a6096ea97b2be6fbc3ae086850cf82a77553fc97
Damian <dugandamian@gmail.com>
2019-01-31 18:34:52 +0100

    stash

--- client/InstaPic/src/app/picture-components/picture-create/picture-create.component.ts
+++ client/InstaPic/src/app/picture-components/picture-create/picture-create.component.ts
@@ -1,9 +1,24 @@
-declare var require: any;
+// getExif(image) {
+//   exif.EXIF.getData(image, function() {
+//     let make = exif.EXIF.getTag(this, "Make");
+//     let model = exif.EXIF.getTag(this, "Model");
+//     console.log(make, model);
+//   });
+
+//   exif.EXIF.getData(image, function() {
+//     let allMetaData = exif.EXIF.getAllTags(this);
+//     console.log(allMetaData);
+//   });
+// }
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
 import { NgForm } from "@angular/forms";
 import { PictureService } from "./picture.service";
-const exif = require("exif-js");
+
+// import the file uploader plugin
+import { FileUploader } from "ng2-file-upload/ng2-file-upload";
+// define the constant url we would be uploading to.
+const URL = "http://localhost:8000/picture/upload";
 
 @Component({
   selector: "app-picture-create",
@@ -14,27 +29,40 @@ const exif = require("exif-js");
 export class PictureCreateComponent implements OnInit {
   showSuccessMessage: boolean;
   serverErrorMessages: string;
+
+  // declare a property called fileuploader and assign it to an instance of a new fileUploader.
+  // pass in the Url to be uploaded to, and pass the itemAlais, which would be the name of the //file input when sending the post request.
+  public uploader: FileUploader = new FileUploader({
+    url: URL,
+    itemAlias: "photo"
+  });
+  // This is the default title property created by the angular cli. Its responsible for the app works
+  title = "app works!";
+
   constructor(private pictureService: PictureService, private router: Router) {}
 
-  ngOnInit() {}
-
-  // getExif(image) {
-  //   exif.EXIF.getData(image, function() {
-  //     let make = exif.EXIF.getTag(this, "Make");
-  //     let model = exif.EXIF.getTag(this, "Model");
-  //     console.log(make, model);
-  //   });
-
-  //   exif.EXIF.getData(image, function() {
-  //     let allMetaData = exif.EXIF.getAllTags(this);
-  //     console.log(allMetaData);
-  //   });
-  // }
+  ngOnInit() {
+    // override the onAfterAddingfile property of the uploader so it doesn't authenticate with //credentials.
+    this.uploader.onAfterAddingFile = file => {
+      file.withCredentials = false;
+    };
+    // overide the onCompleteItem property of the uploader so we are
+    // able to deal with the server response.
+    this.uploader.onCompleteItem = (
+      item: any,
+      response: any,
+      status: any,
+      headers: any
+    ) => {
+      console.log("ImageUpload:uploaded:", item, status, response);
+    };
+  }
 
   onSubmit(form: NgForm) {
     this.pictureService.postPicture(form.value).subscribe(
       res => {
-        console.log("form value: " + form.value.image);
+        console.log("res" + res);
+        // console.log(form.value);
         this.showSuccessMessage = true;
       },
       err => {
