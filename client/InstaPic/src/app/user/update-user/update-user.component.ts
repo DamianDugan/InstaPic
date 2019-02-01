ce286dcb45cfc08900758adf5c1462b20f7d7dfe
BeverleyB <beverley.beranger@gmail.com>
2019-02-01 12:13:49 +0100

    correct the update user function in back end

--- client/InstaPic/src/app/user/update-user/update-user.component.ts
+++ client/InstaPic/src/app/user/update-user/update-user.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { UserService } from "src/app/shared/user.service";
+import { JwtHelperService } from "@auth0/angular-jwt";
 
 @Component({
-  selector: 'app-update-user',
-  templateUrl: './update-user.component.html',
-  styleUrls: ['./update-user.component.css']
+  selector: "app-update-user",
+  templateUrl: "./update-user.component.html",
+  styleUrls: ["./update-user.component.css"]
 })
 export class UpdateUserComponent implements OnInit {
+  users: Object;
+  helper: JwtHelperService;
 
-  constructor() { }
+  constructor(private userService: UserService) {}
 
   ngOnInit() {
+    // const token = this.userService.getToken();
+    // const decodedToken = this.helper.decodeToken(token);
+    // const idDecode = decodedToken._id;
+    // this.userService.showUser(idDecode).subscribe(user => {
+    //   this.users = user;
+    // });
   }
 
+  updateProfile() {
+    // this.userService.updateUser(this.users).subscribe(newUser => {
+    //   this.users = newUser;
+    // });
+  }
 }
