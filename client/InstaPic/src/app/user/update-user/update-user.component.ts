e44b3d65ffc037c07a9b2d117fb24a82ba9ff9cc
BeverleyB <beverley.beranger@gmail.com>
2019-02-04 14:32:56 +0100

    add form for update user

--- client/InstaPic/src/app/user/update-user/update-user.component.ts
+++ client/InstaPic/src/app/user/update-user/update-user.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { UserService } from "src/app/shared/user.service";
 import { JwtHelperService } from "@auth0/angular-jwt";
+import { NgForm } from "@angular/forms";
 
 @Component({
   selector: "app-update-user",
@@ -9,22 +10,25 @@ import { JwtHelperService } from "@auth0/angular-jwt";
 })
 export class UpdateUserComponent implements OnInit {
   users: Object;
-  helper: JwtHelperService;
+  helper = new JwtHelperService();
 
   constructor(private userService: UserService) {}
 
   ngOnInit() {
-    // const token = this.userService.getToken();
-    // const decodedToken = this.helper.decodeToken(token);
-    // const idDecode = decodedToken._id;
-    // this.userService.showUser(idDecode).subscribe(user => {
-    //   this.users = user;
-    // });
+    const token = this.userService.getToken();
+    const decodedToken = this.helper.decodeToken(token);
+    const idDecode = decodedToken._id;
+    console.log(this.userService);
+    this.userService.showUser(idDecode).subscribe(user => {
+      this.users = user;
+    });
   }
 
-  updateProfile() {
-    // this.userService.updateUser(this.users).subscribe(newUser => {
-    //   this.users = newUser;
-    // });
+  onSubmit(form: NgForm) {
+    console.log(this.userService);
+    this.userService.updateUser(this.userService).subscribe(newUser => {
+      console.log(newUser);
+      this.users = newUser;
+    });
   }
 }
