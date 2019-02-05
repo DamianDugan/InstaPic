12534473c449a908da18bf39c85f5cb753942d8c
BeverleyB <beverley.beranger@gmail.com>
2019-02-05 18:47:28 +0100

    begin to work on the like functionnality

--- client/InstaPic/src/app/picture-components/picture-show/picture-show.component.ts
+++ client/InstaPic/src/app/picture-components/picture-show/picture-show.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-import { Picture } from '../picture-create/picture.model';
-import { PictureService } from '../picture-create/picture.service';
+import { Component, OnInit } from "@angular/core";
+import { Picture } from "../picture-create/picture.model";
+import { PictureService } from "../picture-create/picture.service";
+import { UserService } from "src/app/shared/user.service";
+import { JwtHelperService } from "@auth0/angular-jwt";
 
 @Component({
-  selector: 'app-picture-show',
-  templateUrl: './picture-show.component.html',
-  styleUrls: ['./picture-show.component.css']
+  selector: "app-picture-show",
+  templateUrl: "./picture-show.component.html",
+  styleUrls: ["./picture-show.component.css"]
 })
 export class PictureShowComponent implements OnInit {
   pictures: Picture[] = [];
-  constructor(private pictureService: PictureService) {}
+  helper = new JwtHelperService();
+  constructor(
+    private pictureService: PictureService,
+    private userService: UserService
+  ) {}
 
   ngOnInit() {
     this.showPictures();
@@ -20,5 +26,16 @@ export class PictureShowComponent implements OnInit {
       console.log(pictures);
       this.pictures = pictures as Picture[];
     });
+  }
+
+  private addToFav(id) {
+    let token = this.userService.getToken();
+    let decoded = this.helper.decodeToken(token);
+    const decodeIdUser = decoded._id;
+    console.log(decodeIdUser);
+
+    // this.userService.showUser(decoded._id).subscribe(res => {
+    //   this.userService;
+    // });
   }
 }
